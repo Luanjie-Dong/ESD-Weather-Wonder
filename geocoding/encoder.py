20c0d44ceae3f223add865cb140d0ad779fcf839
Luanjie-Dong <164493154+Luanjie-Dong@users.noreply.github.com>
2025-03-06 12:11:53 +0800

    UPDATE GEOCODER

--- geocoding/encoder.py
+++ geocoding/encoder.py
@@ -1,13 +1,93 @@
 import requests
+from dotenv import load_dotenv
+import os
+from urllib.parse import quote
+load_dotenv()
 
 
 def encode_location_by_name(location_name):
-    pass
 
+    url = "https://api.opencagedata.com/geocode/v1/json?"
+    api_key = os.environ.get("GEOCODING_API_KEY")
+    params = {
+        "q": location_name,
+        "key": api_key
+    }
+  
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  
 
-def encode_location_by_zip(zip_code):
-    pass
+        try:
+            location_data = response.json()
+        except ValueError:
+            print(f"Invalid JSON response for {location_name}")
+            return None
+        
 
+        if "results" in location_data and len(location_data["results"]) > 0:
+            result = location_data["results"][0]
+            return {
+                "latitude": result["geometry"]["lat"],
+                "longitude": result["geometry"]["lng"],
+                "country": result.get("components", {}).get("country", "Unknown"),
+                "county": result["components"].get("county", ""),
+                "formatted_location": result['formatted']
+            }
+        else:
+            print(f"No results found for {location_name}")
+            return None
+        
+
+    except requests.exceptions.RequestException as e:
+            print(f"Error encoding location data for {location_name}: {e}")
+
+    
 
 def decode_location(lat,lon):
-    pass
+    url = "https://api.opencagedata.com/geocode/v1/json?"
+    api_key = os.environ.get("GEOCODING_API_KEY")
+
+    lat_lon = f"{lat},{lon}"
+    encoded_lat_lon = quote(lat_lon)
+
+    params = {
+        "q": encoded_lat_lon,
+        "key": api_key
+    }
+  
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  
+
+        try:
+            location_data = response.json()
+        except ValueError:
+            print(f"Invalid JSON response for {lat} , {lon}")
+            return None
+        
+
+        if "results" in location_data and len(location_data["results"]) > 0:
+            result = location_data["results"][0]
+            return {
+                "location": result.get("formatted","")
+            }
+        else:
+            print(f"No results found for {lat} , {lon}")
+            return None
+        
+
+    except requests.exceptions.RequestException as e:
+            print(f"Error encoding location data for {lat} , {lon}: {e}")
+
+
+if __name__=="__main__":
+    # lat = "-31.9842784"
+    # lon = "115.7541052"
+    # decoded = decode_location(lat,lon)
+    # print(decoded)
+
+
+    location = "57 Margaret St Cottesloe"
+    encoded = encode_location_by_name(location)
+    print(encoded)
--- geocoding/main.py
+++ geocoding/main.py
@@ -1,2 +1,55 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  
+from encoder import encode_location_by_name , decode_location
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/encode",methods=['POST'])
+def encode_location_func():
+
+    """
+    Input: Address/Location --> E.g Singapore / 57 Margaret St Cottesloe
+    Output: Country , Town , Latitude , Longitude , formatted location 
+    """
+
+    data = request.get_json()
+    if not data:
+            return jsonify({"error": "No JSON data provided"}), 400
+    
+    location_data = data.get("location","")
+    encoded_data = encode_location_by_name(location_data)
+
+    if encoded_data != "":
+        return encoded_data
+    else:
+        return f"Error encoding {location_data}", 404
+
+
+@app.route("/decode",methods=['POST'])
+def decode_location_func():
+
+    """
+    Input: Latitude , Longitude 
+    Output: Address/Location --> E.g Singapore / 57 Margaret St Cottesloe
+    """
+
+    data = request.get_json()
+    if not data:
+            return jsonify({"error": "No JSON data provided"}), 400
+    
+    location_lat = data.get("lat","")
+    location_lon = data.get("lon","")
+
+    decoded_data = decode_location(location_lat,location_lon)
+
+    if decoded_data != "":
+        return decoded_data
+    else:
+        return f"ERROR decoding with {location_lat} , {location_lon}", 404
+
+
+
+if __name__=="__main__":
+     app.run('0.0.0.0',port=5000,debug=True)
