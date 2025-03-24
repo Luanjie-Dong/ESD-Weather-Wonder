5399ba6e5ef4711a2a135a21aa88f2ab7a472980
Luanjie-Dong <164493154+Luanjie-Dong@users.noreply.github.com>
2025-03-24 15:47:49 +0800

    updates on frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -218,7 +218,11 @@ export default function LandingPage() {
                 </form>
                 {loading && (
                   <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+<<<<<<< Updated upstream
+                    <p className="text-white text-lg font-semibold">Logging in..</p>
+=======
                     <p className="text-white text-lg font-semibold">Creating Account...</p>
+>>>>>>> Stashed changes
                   </div>
                 )}
               </TabsContent>
